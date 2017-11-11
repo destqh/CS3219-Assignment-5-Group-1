1e91b7c185282dd70e28df64814bbaf8bb75d4a8
Musa <ghurabah93@hotmail.com>
2017-11-11 23:42:53 +0800

    remove unnecessary stuff

--- web_server/index.php
+++ web_server/index.php
@@ -17,14 +17,7 @@
 		<span class="w3-large">Correlate and visualize data of conference information</span>
 		<p><a href="#help" class="w3-button w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off">Learn more and start today</a></p>
 	</div>
-	<div class="w3-display-bottomleft w3-text-grey w3-large" style="padding:24px 48px">
-		<i class="fa fa-facebook-official w3-hover-opacity"></i>
-		<i class="fa fa-instagram w3-hover-opacity"></i>
-		<i class="fa fa-snapchat w3-hover-opacity"></i>
-		<i class="fa fa-pinterest-p w3-hover-opacity"></i>
-		<i class="fa fa-twitter w3-hover-opacity"></i>
-		<i class="fa fa-linkedin w3-hover-opacity"></i>
-	</div>
+	
 </header>
 
 <!-- Help Section -->
